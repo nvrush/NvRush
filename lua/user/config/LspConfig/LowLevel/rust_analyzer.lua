f66e956587a0cc0a418e6b8b8880d57ea134b0ca
nvrush <madharchodambani@gmail.com>
2026-01-03 20:17:05 +0530

    Optimised Rust-analyzer for termux and removed lag

--- lua/user/config/LspConfig/LowLevel/rust_analyzer.lua
+++ lua/user/config/LspConfig/LowLevel/rust_analyzer.lua
@@ -1,44 +1,89 @@
 local lspconfig = require("lspconfig")
 
-
 lspconfig.rust_analyzer.setup({
     flags = {
-        allow_incremental_sync = true,
-        debounce_text_changes = 500 -- Wait 150ms after typing stops
+        debounce_text_changes = 300,
     },
     settings = {
         ["rust-analyzer"] = {
+            -- IMPORTANT: avoid Cargo path mismatches
+            checkOnSave = {
+                enable = true,
+                command = "check", -- Set to check use clippy instead
+            },
+
             rustc = {
                 source = "discover",
             },
-            files = {
-                excludeDirs = { ".git", "target", "node_modules" }, -- Don't scan these
-            },
+
             cargo = {
+                allFeatures = false,
                 buildScripts = {
                     enable = false,
                 },
-                allFeatures = false,
             },
-            checkOnSave = {
-                enable = true,
-                command = "clippy"
-            },
+
             procMacro = {
-                enable = true,
-                attribute = {
-                    enable = false,
+                enable = false, -- For termux optimization
+            },
+
+            files = {
+                excludeDirs = {
+                    ".git",
+                    "target",
+                    "node_modules",
                 },
             },
+
             diagnostics = {
                 enable = true,
                 experimental = {
                     enable = false,
-                }
+                },
             },
         },
     },
 })
+
+-- local lspconfig = require("lspconfig")
+-- lspconfig.rust_analyzer.setup({
+--     flags = {
+--         allow_incremental_sync = true,
+--         debounce_text_changes = 500 -- Wait 150ms after typing stops
+--     },
+--     settings = {
+--         ["rust-analyzer"] = {
+--             rustc = {
+--                 source = "discover",
+--             },
+--             files = {
+--                 excludeDirs = { ".git", "target", "node_modules" }, -- Don't scan these
+--             },
+--             cargo = {
+--                 buildScripts = {
+--                     enable = false,
+--                 },
+--                 allFeatures = false,
+--             },
+--             checkOnSave = {
+--                 enable = true,
+--                 command = "clippy"
+--             },
+--             procMacro = {
+--                 enable = true,
+--                 attribute = {
+--                     enable = false,
+--                 },
+--             },
+--             diagnostics = {
+--                 enable = true,
+--                 experimental = {
+--                     enable = false,
+--                 }
+--             },
+--         },
+--     },
+-- })
 
 vim.lsp.handlers["workspace/diagnostic/refresh"] = function(_, _, ctx)
     return vim.NIL
